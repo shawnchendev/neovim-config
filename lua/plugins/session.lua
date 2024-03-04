efddcf655afa6ca2db6338110ab2e8a68596a33b
Shawn Chen <meishang.chen@shopify.com>
2024-03-04 09:48:07 -0330

    fix auto session

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,11 +1,6 @@
 return {
-	"rmagatti/auto-session",
-	config = function()
-		require("auto-session").setup({
-			log_level = "error",
-			auto_session_enable_last_session = true,
-      auto_session_suppress_dirs = { "~/", "~/github/", "~/Downloads", "/" },
-
-		})
-	end,
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup()
+  end,
 }
